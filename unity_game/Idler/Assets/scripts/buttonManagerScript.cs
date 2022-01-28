00c98e1ef1f2286bdb867a8abe3ee2aa0312f607
Rydia T <rydia.a.titus@gmail.com>
2022-01-27 20:31:52 -0600

    Pt. 2

--- unity_game/Idler/Assets/scripts/buttonManagerScript.cs
+++ unity_game/Idler/Assets/scripts/buttonManagerScript.cs
@@ -1,9 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class buttonManagerScript : MonoBehaviour
 {
+    public int n;
+    public Text buttonNum;
+    public void OnButtonPress()
+    {
+        n++;
+        buttonNum.text = "Button has been pressed " + n + " times.";
+    }
     // Start is called before the first frame update
     void Start()
     {
